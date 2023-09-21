439239e53f389a5f1d20f51388c75f8801eb47b4
snhrayk <144076726+snhrayk@users.noreply.github.com>
2023-09-22 01:23:29 +0900

    for03まで完了

---
+++ 03_for/js/for03.js
@@ -0,0 +1,14 @@
+// 自分の答え
+// for(let i=0; i<=20; i++){
+//     const no = i * 2;
+//     if(no <= 20){
+//         console.log(no);
+//     }
+// }
+
+// いくちゃん本田くんの答え
+for(let i=0; i<=20; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
