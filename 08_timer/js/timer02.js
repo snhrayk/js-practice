e9af00cc7a02c1f2b214e305e8c7fbde77628db5
snhrayk <144076726+snhrayk@users.noreply.github.com>
2023-11-28 12:13:54 +0900

    timer02すぐに1が出てこないようにする方法がわからない

--- 08_timer/js/timer02.js
+++ 08_timer/js/timer02.js
@@ -1,21 +1,15 @@
-const counter = document.getElementById("counter");
+const list = document.getElementById("counter");
 let count = 0;
 const duration = 1000;
 
 function setTimer() {
-    count++;
     if (count < 30) {
-        counter.innerHTML += count;
-        //()は即時実行するので、イベントハンドラとしてsetTimerをセットするため()は不要
+        count++;
+        list.innerHTML += count;
         setTimeout(setTimer, duration);
     }
     else {
-        // countが10になるとsetTimeout終了
         return false;
     }
 }
 setTimer();
-// for (let count = 0; count < counter.length; count++) {
-//     counter.innerHTML += count;
-//     setTimeout(setTimer, duration);
-// }
