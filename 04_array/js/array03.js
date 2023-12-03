7805e9c4971c23a183d7fb76f79d523d40dd7663
snhrayk <144076726+snhrayk@users.noreply.github.com>
2023-12-03 23:50:32 +0900

    array03完了

---
+++ 04_array/js/array03.js
@@ -0,0 +1,8 @@
+// 配列 web を作成し、
+// WD1A / WD2A を、各要素に入れて、コンソール画面に表示すること。
+// その後、WD3Aを末尾に追加し、改めてコンソール画面にWD3Aが追加されたかを表示すること。
+
+let web = ["WD1A", " WD2A"];
+console.log(web);
+web.push("WD3A");
+console.log(web);
