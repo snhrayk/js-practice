bfb90fbef14d894cfb84414a7cd26909cfca4e5f
snhrayk <144076726+snhrayk@users.noreply.github.com>
2023-12-03 23:47:08 +0900

    array02完了

---
+++ 04_array/js/array02.js
@@ -0,0 +1,10 @@
+// 配列 ramen を作成し、
+// sio / miso / tonkotu / nibosi / shoyu / jirou を、各要素に入れましょう。
+// コンソール画面には下記のメッセージを表示すること。
+
+// 「ここのお店のラーメンは全部でXXX種類、おすすめはmisoです」
+
+// ※おすすめは miso ですのmisoは配列の要素を使って表示
+
+let ramen = ["sio", "miso", "tonkotu", "nibosi", "shoyu", "jirou"];
+console.log("ここのお店のラーメンは全部で" + ramen.length + "種類、おすすめは" + ramen[1] + "です");
