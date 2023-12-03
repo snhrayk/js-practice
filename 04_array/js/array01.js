bada5d27b8c9cf6bf4bf3ed85ec28f9b3283b439
snhrayk <144076726+snhrayk@users.noreply.github.com>
2023-12-03 23:39:30 +0900

    array01完了

---
+++ 04_array/js/array01.js
@@ -0,0 +1,16 @@
+// let arr1 = new Array();
+// console.log(arr1);
+
+// arr1[0] = "html";
+// arr1[1] = "css";
+// arr1[2] = "js";
+// console.log(arr1);
+
+// let arr2 = [];
+// arr2 = ["ps", "ai", "xd"];
+// console.log(arr2);
+
+let fruits = ["apple", "banana", "lemon"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
