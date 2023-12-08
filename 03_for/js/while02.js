981965eacddbdae189a24b953e7094b612db42a2
snhrayk <144076726+snhrayk@users.noreply.github.com>
2023-12-08 23:41:45 +0900

    while02完了

---
+++ 03_for/js/while02.js
@@ -0,0 +1,13 @@
+// let count = 2;
+// do {
+//     console.log(`${count}回の攻撃開始`);
+//     count--;
+// }
+// while (count > 10);
+
+let count = 2;
+do {
+    console.log(`${count}回の攻撃開始`);
+    count--;
+}
+while (count > 0);
