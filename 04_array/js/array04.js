c19e435ef78c952dbb35f8375b0a2a8095295498
snhrayk <144076726+snhrayk@users.noreply.github.com>
2023-12-10 23:42:25 +0900

    array04完了

---
+++ 04_array/js/array04.js
@@ -0,0 +1,9 @@
+// 配列 ecccomp を作成し、
+// GameCreative / IT / Design を、各要素に入れて、コンソール画面に表示すること。
+// 配列 ecccompの末尾を削除して改めてコンソール画面に表示し、Designが消えているかを確認すること。
+
+let ecccomp = ["GameCreative", "IT", "Design"];
+console.log(ecccomp);
+
+ecccomp.pop();
+console.log(ecccomp);
