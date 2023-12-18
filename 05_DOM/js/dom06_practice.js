bab799ef569a03c951326eb45b91196c23a93e00
snhrayk <144076726+snhrayk@users.noreply.github.com>
2023-12-18 14:14:43 +0900

    dom06完了

---
+++ 05_DOM/js/dom06_practice.js
@@ -0,0 +1,10 @@
+const main = document.getElementById("main");
+const sub = document.getElementById("sub");
+const soup = document.getElementById("soup");
+const side = document.getElementById("side");
+const submit = document.getElementById("submit");
+const menu = document.getElementById("menu");
+
+submit.onclick = () => {
+    menu.innerText = "注文内容は" + main.value + "、" + sub.value + "、" + soup.value + "、" + side.value + "です。";
+}
