9a9dbe6bb450f6dcfa1306b7867503c88e73a6f6
snhrayk <144076726+snhrayk@users.noreply.github.com>
2023-12-22 23:59:23 +0900

    dom10完了

---
+++ 05_DOM/js/dom09_practice.js
@@ -0,0 +1,2 @@
+const btnSmall = document.querySelector("btn-small");
+const btnLarge = document.querySelector("btn-large");
---
+++ 05_DOM/js/dom10.js
@@ -0,0 +1,7 @@
+const tr = document.querySelectorAll("tr");
+const name = document.querySelectorAll(".name");
+console.log(tr.name);
+
+for (let i = 0; i < name.length; i++) {
+    console.log(tr[i]);
+} 
